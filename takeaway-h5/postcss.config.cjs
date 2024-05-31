c30ad3857d1ec7afc1c5005364e5f3d9162235ca
MothOfFire <1748972220@qq.com>
2024-05-31 15:19:17 +0800

    fix:修复了rem适配无法生效的BUG

---
+++ takeaway-h5/postcss.config.cjs
@@ -0,0 +1,20 @@
+// rem 适配移动端配置
+
+module.exports = {
+  plugins: {
+    autoprefixer: {
+      overrideBrowserslist: [
+        "Android >= 4.0",
+        "iOS >= 7.0",
+        "Chrome > 31",
+        "ff > 31",
+        "ie >= 8",
+      ],
+    },
+    "postcss-pxtorem": {
+      rootValue: 37.5, // 1rem 等于多少像素
+      propList: ["*"],
+      selectorBlackList: [],
+    },
+  },
+};
